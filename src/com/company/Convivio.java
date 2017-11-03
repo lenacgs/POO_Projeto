95497f3c40db830804ce295840989aff673bd59d
lenacgs <lenacgs@gmail.com>
2017-11-03 19:11:22 +0000

    mudancas de acordo com o feedback do professor

--- src/com/company/Convivio.java
+++ src/com/company/Convivio.java
@@ -8,7 +8,6 @@ import java.lang.*;
 public class Convivio {
     public ArrayList<Pessoa> inscritos = new ArrayList<Pessoa>();
     public ArrayList<Local> locais = new ArrayList<Local>();
-    private ComunidadeDEI comunity = new ComunidadeDEI();
 
     public Convivio() {
     }
@@ -40,7 +39,6 @@ public class Convivio {
         return receita;
     }
 
-
     private void sortLocaisInscricoes() {
         Local toMove;
         for (int i=0; i<locais.size(); i++) {
@@ -54,7 +52,6 @@ public class Convivio {
         }
     }
 
-
     private void printLocais() {
         sortLocaisInscricoes();
 
@@ -65,7 +62,6 @@ public class Convivio {
                 System.out.println(" ------ " + ((Bar) locais.get(i)).getLotacao());
         }
     }
-
 
     private void inscrevePessoa(Pessoa p) {
         inscritos.add(p);
@@ -92,50 +88,5 @@ public class Convivio {
         }
     }
 
-    public Pessoa login() {
-        System.out.println("username ('r' para registar novo user): ");
-        Pessoa p;
-        Scanner sc = new Scanner(System.in);
-        String username = sc.nextLine();
-        if (username.equals("r")) p = comunity.newPessoa();
 
-        else {
-            p = comunity.procuraPessoa(username);
-            if (p == null)
-                p = comunity.newPessoa();
-        }
-        return p;
-
-    }
-
-
-    private static boolean debug = true; //para testes
-    public static void main(String[] args) {
-
-        if (debug) {
-
-            ComunidadeDEI comunity = new ComunidadeDEI();
-            Convivio convivio = new Convivio();
-            Local[] v = new Local[4];
-            v[0] = new Jardim("1233:42:5", "Jardim da Sereia", 25);
-            v[1] = new Bar("2136:17623:434", "Moelas", 100, 1, 40);
-            v[2] = new Exposicao("312:234:2937468", "Trabalhos de Design", "Pintura", 5);
-            String[] desportos = {"Futebol", "Basquetebol", "Ballet"};
-            v[3] = new AreaDesportiva("123:234:234", "Campo de futebol", desportos);
-
-            for (int i = 0; i < v.length; i++)
-                convivio.locais.add(v[i]);
-
-            Pessoa[] p = new Pessoa[5];
-            p[0] = new Aluno("Boémio", "1234", "josepaulo", 1, "medicina");
-            p[1] = new Professor("Poupadinho", "1234", "mariajose", 2, "catedrático");
-            p[2] = new Funcionario("boémio", "1234", "antoniocosta", 3, "tempo inteiro");
-            p[3] = new Professor("boemio", "1234", "marilia", 4, "contratado");
-            p[4] = new Aluno("poupadinho", "1234", "inessantos", 5, "design");
-
-            for (int i = 0; i < p.length; i++)
-                comunity.pessoas.add(p[i]);
-
-        }
-    }
 }
